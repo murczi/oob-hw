0a4702c5379b8ef7e19be5cdfbd3034ff7d57505
Shuji <marcellalter@gmail.com>
2024-05-08 16:11:04 +0200

    Szoba származtatott osztályok

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ class Szoba:
     def __init__(self, ar, szobaszam):
         self.ar = ar
         self.szobaszam = szobaszam
+
+
+class Egyagyasszoba(Szoba):
+    def __init__(self):
+        self.ar = 10
+        self.szobaszam = 1
+
+
+class Ketagyasszoba(Szoba):
+    def __init__(self):
+        self.ar = 10
+        self.szobaszam = 1
